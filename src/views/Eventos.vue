de2a7eeb54e9c6ec9b4647c7103c2bdbb779cbd9
Cristian Alejandro Ramirez Holguin <cristian.ramirez3@ibm.com>
2020-06-04 23:47:56 -0500

    Update new webinar

--- src/views/Eventos.vue
+++ src/views/Eventos.vue
@@ -1,33 +1,53 @@
 <template>
-   <v-layout justify-center align-center row wrap>
+  <v-layout justify-center align-center row wrap>
+    <v-container fluid>
+      <v-row>
+        <v-col cols="6">
+          <v-carousel
+          :show-arrows="false"
+          height="350"
+          cycle>
+          <v-carousel-item
+            contain
+            v-for="(item,i) in events"
+            :key="i"
+            :src="item.src"
+          ></v-carousel-item>
+        </v-carousel>
+        <v-list two-line>
+          <v-list-item>
+            <v-list-item-avatar>
+              <v-img src="https://www.themsphub.com/app/uploads/2017/11/IBM-Cloud-Logo.png"></v-img>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title>IBM Cloud Team</v-list-item-title>
+              <v-list-item-subtitle>Author</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-btn text color="blue" to="/registroev">REGISTRATE AQUI</v-btn>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
+      </v-col>
 
-  <v-flex xs12 md12>
-  </v-flex>
-  <v-spacer/>
-    <v-flex
-    md6
-    v-for="(item,i) in events"
-    :key="i">
-    <v-card>
-        <v-img
-          :src="item.src"
-        ></v-img>
-
-        <v-card-title primary-title>
-            <div class="title"> {{ item.details }} </div>
-        </v-card-title>
-
-        <v-card-actions>
-          <v-btn text color="blue" v-if="item.route != null"
-          :to="item.route">REGISTRATE AQUI</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
+      <v-col cols="6">
+          <v-sheet height="500">
+            <v-calendar
+              color="primary"
+              :now=today
+              :value=today
+              :events="events"
+            ></v-calendar>
+          </v-sheet>
+        </v-col>
+    </v-row>
+  </v-container>
   </v-layout>
-
 </template>
+
 <script>
 import image from "@/assets/Watson Asistant.jpg"
+import image2 from "@/assets/Red Hat Open Shift on Cloud.jpg"
  
 
   export default {
@@ -39,7 +59,7 @@ import image from "@/assets/Watson Asistant.jpg"
         
         valor:null,
         date:new Date(),
-        today:'2019-12-01',
+        today:'2020-06-04',
        
         events: [
         {
@@ -48,6 +68,13 @@ import image from "@/assets/Watson Asistant.jpg"
           route:"/registroev",
           start: '2020-04-28',
           src: image,
+        },
+        {
+          name: 'Red Hat Open Shift on Cloud',
+          details: 'Webinar 06/2020 - IBM Cloud',
+          route:"/registroev",
+          start: '2020-06-10',
+          src: image2,
         },
       ]
       }
